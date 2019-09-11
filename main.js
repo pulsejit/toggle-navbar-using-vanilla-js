b8ae0f1735787a5997282296eb8486a9f0be8adb
Subhajit Chatterjee <subhajitchatterjee425@gmail.com>
2019-09-11 21:31:01 +0530

    width change is fixed and smooth transitons are added

---
+++ main.js
@@ -0,0 +1,27 @@
+let toggleNavStatus = false;
+let toggleNav = function(){
+    let getSidebar = document.querySelector(".nav-sidebar");
+    let getSidebarUl = document.querySelector(".nav-sidebar ul");
+    let getSidebarTitle = document.querySelector(".nav-sidebar span");
+    let getSidebarLinks = document.querySelectorAll(".nav-sidebar a");
+
+    if(toggleNavStatus === false){
+        getSidebarUl.style.visibility="visible";
+        getSidebar.style.width= "272px";
+        getSidebarTitle.style.opacity = ".5";
+        let arrayLength = getSidebarLinks.length;
+        for(let i = 0;i<arrayLength;i++){
+            getSidebarLinks[i].style.opacity="1";
+        }
+        toggleNavStatus = true;
+    }
+
+    else if(toggleNavStatus === true){
+        getSidebarUl.style.visibility="hidden";
+        getSidebar.style.width= "50px";
+        getSidebarTitle.style.opacity = ".5";
+
+        toggleNavStatus = false;
+    }
+
+}
